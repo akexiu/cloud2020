da2713e9ca7338e571c2b4a1d42d0e4ff7bfdd6c
akexiu <akexiuw@163.com>
2020-03-24 23:10:42 +0800

    seata 实现分布式事务

---
+++ cloud2020/seata-account-service2003/src/main/java/com/akexiu/springcloud/alibaba/dao/AccountDao.java
@@ -0,0 +1,10 @@
+package com.akexiu.springcloud.alibaba.dao;
+
+/**
+ * 描述:
+ *
+ * @outhor akexiu
+ * @create 2020-03-19 20:59
+ */
+public class AccountDao {
+}
---
+++ cloud2020/seata-account-service2003/src/main/java/com/akexiu/springcloud/alibaba/domain/Account.java
@@ -0,0 +1,10 @@
+package com.akexiu.springcloud.alibaba.domain;
+
+/**
+ * 描述:
+ *
+ * @outhor akexiu
+ * @create 2020-03-19 20:58
+ */
+public class Account {
+}
